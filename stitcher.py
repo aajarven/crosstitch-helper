7f269a9a39d3c02fe0a5a27229c1507a294e3f14
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2020-04-29 20:19:19 +0300

    Get and validate input for cli, do nothing

--- stitcher.py
+++ stitcher.py
@@ -2,18 +2,24 @@
 Command line interface for cross stitch pattern creation.
 """
 
+import sys
 import click
 
+from conf import palettes
+
 @click.command()
-@click.argument("image")
+@click.argument("image", type=click.File('r'))
 @click.argument("palette")
 def stitchify(image, palette):
     """
     Create a cross-stitch pattern from IMAGE using PALETTE.
 
-    Palette with the same name must be present in conf/palette.py.
+    Palette with the same name must be present in conf/palettes.py.
     """
-    click.echo("stitchifying {} using {}".format(image, palette))
+    colour_codes = getattr(palettes, palette, None)
+    if not colour_codes:
+        click.echo("palette '{}' not found in conf/palettes.py".format(palette))
+        sys.exit(1)
 
 if __name__ == "__main__":
     # pylint: disable=no-value-for-parameter
